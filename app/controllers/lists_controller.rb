d7382a6c959d6a8d4403fc140c58e86d49f82f11
jessicaliar <jfreitas182@gmail.com>
2017-05-22 21:59:15 -0700

    cocoon e ajustes html

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -69,6 +69,7 @@ class ListsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def list_params
-      params.require(:list).permit(:name, :publico, :user_id, :id)
+      params.require(:list).permit(:name, :public, tasks_attributes: [:name, :done, :_destroy])
+      
     end
 end
